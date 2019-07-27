64b7facf687810525043196d01a64b6506808f12
ndurbin <nick.durbin@gmail.com>
2019-07-27 14:34:21 -0400

    Moved over the code for the about page and edited the homepage.

--- javascript/index.js
+++ javascript/index.js
@@ -1,14 +1,24 @@
 const hero = document.querySelector('.hero');
 const slider = document.querySelector('.slider');
-const logo = document.querySelector('.logo');
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-const links = document.querySelectorAll('.nav-a')
+const logo = document.querySelector('#logo');
 const headline = document.querySelector('.headline');
 
-hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('open');
-})
+let swiper = new Swiper('.swiper-container', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 60,
+    stretch: 0,
+    depth: 300,
+    modifier: 1,
+    slideShadows : true,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
 
 const tl = new TimelineMax();
 
@@ -17,7 +27,7 @@ tl.fromTo(hero, 1, { height: "0%" }, { height: "80%", ease: Power2.easeInOut })
     hero,
     1.2,
     { width: "100%" },
-    { width: "80%", ease: Power2.easeInOut }
+    { width: "90%", ease: Power2.easeInOut }
   )
   .fromTo(
     slider,
@@ -26,7 +36,5 @@ tl.fromTo(hero, 1, { height: "0%" }, { height: "80%", ease: Power2.easeInOut })
     { x: "0%", ease: Power2.easeInOut },
     "-=1.2"
   )
-  .fromTo(logo, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.5")
-  .fromTo(headline, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.5");
-
+  .fromTo(headline, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.2");
 
